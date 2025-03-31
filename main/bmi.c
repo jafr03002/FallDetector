d695a9d9b795cf67051a5554e2267d89d47d6e66
frja22sv <129272707+frja22sv@users.noreply.github.com>
2025-03-31 11:56:42 +0200

    file structure

---
+++ main/bmi.c
@@ -0,0 +1,14 @@
+/*
+ *   Source file for bmi methods for reading, writing, initializing
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+
