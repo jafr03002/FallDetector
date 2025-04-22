071625c1482a202b3d40e0db1bd416c58c087179
frja22sv <129272707+frja22sv@users.noreply.github.com>
2025-04-22 11:42:12 +0200

    Working MPU9050 code for gyro and accel data receiving uncommented

--- main/bmi.h
+++ main/bmi.h
@@ -13,6 +13,9 @@
 #include "driver/i2c_master.h"
 
 #define CMD       0x7E
+#define MPU_PWR_MGMT 0x6B
+#define SENSITIVITY 16384 //16 bit signed integer (2^15/2g)
+#define FULL_SCALE_RANGE 250 //250±dps
 
 #define ACC_CONF  0x40
 #define ACC_RANGE 0x41
@@ -26,6 +29,26 @@
 #define ACC_LSB_Z 0x16
 #define ACC_MSB_Z 0x17
 
+#define WHO_AM_I 0x75
+
+#define MPU_ACCEL_XOUT_H 0x3B
+#define MPU_ACCEL_XOUT_L 0x3C
+
+#define MPU_ACCEL_YOUT_H 0x3D
+#define MPU_ACCEL_YOUT_L 0x3E
+
+#define MPU_ACCEL_ZOUT_H 0x3F
+#define MPU_ACCEL_ZOUT_L 0x40
+
+#define MPU_GYRO_XOUT_H 0x43
+#define MPU_GYRO_XOUT_L 0x44
+
+#define MPU_GYRO_YOUT_H 0x45
+#define MPU_GYRO_YOUT_L 0x46
+
+#define MPU_GYRO_ZOUT_H 0x47
+#define MPU_GYRO_ZOUT_L 0x48
+
 #define GYR_LSB_X 0x0C
 #define GYR_MSB_X 0x0D
 #define GYR_LSB_Y 0x0E
@@ -37,13 +60,7 @@
 
 #define DATA_LENGTH 100
 
-extern float ACC_X;
-extern float ACC_Y;
-extern float ACC_Z;;
-
-extern float GYR_X;
-extern float GYR_Y;
-extern float GYR_Z;
+#define I2C_MASTER_TIMEOUT_MS       1000
 
 typedef enum{
    AXIS_X,
@@ -52,10 +69,11 @@ typedef enum{
 }Axis;
 
 void BMI160_Init(i2c_master_dev_handle_t handle);
-void BMI160_ReadRegister(i2c_master_dev_handle_t handle, uint8_t regg_adress, uint8_t * data);
-void BMI160_WriteRegister(i2c_master_dev_handle_t handle, uint8_t regg_adress, uint8_t data_buf);
+esp_err_t BMI160_ReadRegister(i2c_master_dev_handle_t handle, uint8_t regg_adress, uint8_t * data, size_t len);
+esp_err_t BMI160_WriteRegister(i2c_master_dev_handle_t handle, uint8_t regg_adress, uint8_t data_buf);
 float BMI160_ReadAccel(i2c_master_dev_handle_t handle, Axis axis);
 float BMI160_ReadGyro(i2c_master_dev_handle_t handle, Axis axis);
-void BMI160_PrintValues(void);
+int16_t MPU_ReadAccel(i2c_master_dev_handle_t handle, Axis axis);
+int16_t MPU_ReadGyro(i2c_master_dev_handle_t handle, Axis axis);
 
 
--- main/fall_detecter.c
+++ main/fall_detecter.c
@@ -14,58 +14,118 @@
 #include "esp_system.h"
 #include "esp_mac.h"
 #include "driver/i2c_master.h"
+#include "driver/i2c_slave.h"
 #include "bmi.h"
 
-#define SDA 22
-#define SCL 21
+#define SDA 21
+#define SCL 22
 
-float ACC_X = 0;
-float ACC_Y = 0;
-float ACC_Z = 0;
-float GYR_X = 0;
-float GYR_Y = 0;
-float GYR_Z = 0;
+int16_t ACC_X = 0;
+int16_t ACC_Y = 0;
+int16_t ACC_Z = 0;
 
-void detector_task(void *args){
+int16_t GYR_X = 0;
+int16_t GYR_Y = 0;
+int16_t GYR_Z = 0;
 
+TaskHandle_t BMI180ReadHandle = NULL;
+TaskHandle_t CheckAddressHandle = NULL;
+
+uint8_t who_buf[1];
+
+float g = 9.81;
+
+float ax_real = 0;
+float ay_real = 0;
+float az_real = 0;
+
+float ax_m_per_s = 0;
+float ay_m_per_s = 0;
+float az_m_per_s = 0;
+
+float gx_real = 0;
+float gy_real = 0;
+float gz_real = 0;
+
+static void i2c_master_init_bus(i2c_master_bus_handle_t *bus_handle){
     i2c_master_bus_config_t i2c_mst_config = {
         .clk_source = I2C_CLK_SRC_DEFAULT,
-        .i2c_port = I2C_NUM_0,
+        .i2c_port = I2C_NUM_1,
         .scl_io_num = SCL,
         .sda_io_num = SDA,
         .flags.enable_internal_pullup = true,
         .glitch_ignore_cnt = 7,
     };
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, bus_handle));
+}
 
-    i2c_master_bus_handle_t bus_handle;
-    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
-
-
-    i2c_device_config_t dev_cfg = {
+static void i2c_master_init_handle(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_handle_t *dev_handle, uint8_t address){
+    i2c_device_config_t dev_config = {
         .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-        .device_address = DEV_ADDR,
+        .device_address = address,
         .scl_speed_hz = 100000,
     };
-    
-    i2c_master_dev_handle_t dev_handle;
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle));
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(*bus_handle, &dev_config, dev_handle));
+}
 
-    BMI160_Init(dev_handle);
+void check_address_task(void *arg){
+    i2c_master_bus_handle_t *bus_handle = (i2c_master_bus_handle_t*) arg;
+    while(1){
+        esp_err_t err;
+        // Using i2c_master_probe on a for loop going through all available address
+        for(uint8_t i = 3; i < 0x78; i++){
+            err = i2c_master_probe(*bus_handle, i, 1000);
+            if (err == ESP_OK){
+                printf("I2C Scanner found I2C device at: 0x%X \n", i);
+            }
+        }
+        printf("I2C_Scanner complete \n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    vTaskDelete(NULL);
+}
+
+void detector_task(void *arg){
+    i2c_master_bus_handle_t bus_handle;
+    i2c_master_dev_handle_t dev_handle;
+    i2c_master_init_bus(&bus_handle);
+
+    i2c_master_init_handle(&bus_handle, &dev_handle, DEV_ADDR);
 
     while(1){
-        ACC_X = BMI160_ReadAccel(dev_handle, AXIS_X);
-        ACC_Y = BMI160_ReadAccel(dev_handle, AXIS_Y);
-        ACC_Z = BMI160_ReadAccel(dev_handle, AXIS_Z);
+        uint8_t wakeup_data = 0x00;
+        BMI160_WriteRegister(dev_handle, MPU_PWR_MGMT, wakeup_data);
 
-        GYR_X = BMI160_ReadGyro(dev_handle, AXIS_X);
-        GYR_Y = BMI160_ReadGyro(dev_handle, AXIS_Y);
-        GYR_Z = BMI160_ReadGyro(dev_handle, AXIS_Z);
+        ACC_X = MPU_ReadAccel(dev_handle, AXIS_X);
+        ACC_Y = MPU_ReadAccel(dev_handle, AXIS_Y);
+        ACC_Z = MPU_ReadAccel(dev_handle, AXIS_Z);
 
-        BMI160_PrintValues();
+        GYR_X = MPU_ReadGyro(dev_handle, AXIS_X);
+        GYR_Y = MPU_ReadGyro(dev_handle, AXIS_Y);
+        GYR_Z = MPU_ReadGyro(dev_handle, AXIS_Z);
+
+        //printf("first A: %d, %d, %d G: %d, %d, %d\n", ACC_X, ACC_Y, ACC_Z, GYR_X, GYR_Y, GYR_Z);
+
+        ax_real = (float)ACC_X/16384.0;
+        ay_real = (float)ACC_Y/16384.0;
+        az_real = (float)ACC_Z/16384.0;
+
+        ax_m_per_s = ax_real*9.81;
+        ay_m_per_s = ay_real*9.81;
+        az_m_per_s = az_real*9.81;
+
+        gx_real = (float)GYR_X/131.0;
+        gy_real = (float)GYR_Y/131.0;
+        gz_real = (float)GYR_Z/131.0;
+
+        printf("Accel(m/s^2): X: %0.2f, Y: %0.2f, Z: %0.2f Gyro(dps): X: %0.1f, Y: %0.1f, Z: %0.1f\n", ax_m_per_s, ay_m_per_s, az_m_per_s, gx_real, gy_real, gz_real);
+    
+        vTaskDelay(1000/portTICK_PERIOD_MS); 
     }
+
 }
 
 void app_main(void)
 {
-    xTaskCreate(&detector_task, "detector", 2048, NULL, 5, NULL); 
+    xTaskCreatePinnedToCore(detector_task, "detector", 4096, NULL, 10, &BMI180ReadHandle, 1); 
 }
