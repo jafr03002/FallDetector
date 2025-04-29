71cad74ef2f6cc9bf427b7c81ea9d58fb29d22b9
Oscar Lindberg <lios22hc@student.ju.se>
2025-04-29 10:13:56 +0200

    dps conversion start

--- visual/ser.py
+++ visual/ser.py
@@ -1,7 +1,21 @@
-import serial
+import serial, threading, time
+from vpython import vector
+from scene import Scene
+
+def dps_conversion(val):
+    time.sleep(1)
+    scene.box.axis = scene.rotate_vector(vector(1, 0, 0), val[0], val[1], val[2])
+    scene.box.up = scene.rotate_vector(vector(0, 1, 0), val[0], val[1], val[2])
 
 with serial.Serial(port=('COM5'), baudrate=115200, timeout=1) as ser:
+    scene = Scene()
     while True:
         line = ser.readline().decode('utf-8').strip()
         values = list(map(float, line.split()))
         print("IMU values: {}", values)
+
+        dps_thread = threading.Thread(target=dps_conversion([values[-1], values[-2], values[-3]]), daemon=True)
+        dps_thread.start()
+
+        scene.update_acc() # vector(värde1, värde2, värde3)
+
